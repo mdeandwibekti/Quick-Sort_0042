724b938aa077ca70f7d7bba71580661ed6aa45ca
mdeandwibekti <mdeandwibekti@gmail.com>
2024-03-28 17:53:57 +0700

    memasukan array

--- Quick Sort_042/Quick Sort_042.cpp
+++ Quick Sort_042/Quick Sort_042.cpp
@@ -7,3 +7,18 @@ int cmp_count = 0; // number of comparasion
 int mov_count = 0; // number of data movements
 int n;
 
+void input() {
+	while (true)
+	{
+		cout << "masukan panjang elemen array: ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nmaksimum panjang array adalah 20" << endl;
+	}
+	cout << "\m---------------" << endl;
+	cout << "\nEnter array element" << endl;
+	cout << "\n---------------" << endl;
+}
